66c2f2cec7074278d3170609aff1f46b55e3824c
lazycoder1 <gautamgsabhahit@gmail.com>
2024-07-01 04:27:39 +0530

    adding stars to repos

--- hooks/eas.tsx
+++ hooks/eas.tsx
@@ -1,37 +1,126 @@
 import { useEffect, useState } from "react";
 import { EAS, Offchain, SchemaEncoder, SchemaRegistry, TransactionSigner, getSchemaUID } from "@ethereum-attestation-service/eas-sdk";
 import { SmartAccountSigner } from "permissionless/accounts";
-import { SmartAccountClient } from "permissionless";
+import { ENTRYPOINT_ADDRESS_V07, SmartAccountClient, providerToSmartAccountSigner } from "permissionless";
 import { ENTRYPOINT_ADDRESS_V07_TYPE } from "permissionless/types/entrypoint";
-import { KernelAccountClient } from "@zerodev/sdk";
+import { KernelAccountClient, createKernelAccount, createKernelAccountClient } from "@zerodev/sdk";
 import { baseSepolia } from "viem/chains";
 import { easAbi } from "./easAbi";
+import { useWallets } from "@privy-io/react-auth";
+import { signerToEcdsaValidator } from "@zerodev/ecdsa-validator";
+import { createPimlicoPaymasterClient, createPimlicoBundlerClient } from "permissionless/clients/pimlico";
+import { createPublicClient, http } from "viem";
 
 const schemaRegistryContractAddress = "0x4200000000000000000000000000000000000020"; // base sepolia
 const schemaRegistry = new SchemaRegistry(schemaRegistryContractAddress);
-const schema = "uint256 eventId, uint8 voteIndex";
+const schema = "string github_url,string maintainer_github_id,string remark,string contributor_github_id";
 const resolverAddress = "0x4200000000000000000000000000000000000020"; // base sepolia
 const eas = "0x4200000000000000000000000000000000000021" as `0x${string}`;
 const revocable = true;
 
 const useEas = () => {
     const [accountClient, setAccountClient] = useState<null | KernelAccountClient<ENTRYPOINT_ADDRESS_V07_TYPE>>(null);
+    const { wallets } = useWallets();
+
+    // useEffect(() => {
+    //     const attest = async () => {
+    //         console.log("account client", accountClient);
+    //         await attestSchema();
+    //     };
+    //     attest();
+    // }, [accountClient]);
     useEffect(() => {
-        const attest = async () => {
-            console.log("account client", accountClient);
-            await attestSchema();
+        const initializeAccount = async () => {
+            try {
+                // Find the embedded wallet and get its EIP1193 provider
+                const embeddedWallet = wallets.find((wallet) => wallet.walletClientType === "privy");
+                if (!embeddedWallet) {
+                    console.error("Embedded wallet not found");
+                    return;
+                }
+                const provider = await embeddedWallet.getEthereumProvider();
+
+                // Use the EIP1193 `provider` from Privy to create a `SmartAccountSigner`
+                const smartAccountSigner = await providerToSmartAccountSigner(provider);
+
+                // Initialize a viem public client on your app's desired network
+                const publicClient = createPublicClient({
+                    transport: http(baseSepolia.rpcUrls.default.http[0]),
+                });
+
+                // Create a ZeroDev ECDSA validator from the `smartAccountSigner` from above and your `publicClient`
+                const ecdsaValidator = await signerToEcdsaValidator(publicClient, {
+                    signer: smartAccountSigner,
+                    entryPoint: ENTRYPOINT_ADDRESS_V07,
+                    kernelVersion: "0.3.0",
+                });
+
+                // Create a Kernel account from the ECDSA validator
+                const account = await createKernelAccount(publicClient, {
+                    plugins: {
+                        sudo: ecdsaValidator,
+                    },
+                    entryPoint: ENTRYPOINT_ADDRESS_V07,
+                    kernelVersion: "0.3.0",
+                });
+
+                const BUNDLER_PAYMASTER_URL = "https://api.pimlico.io/v2/84532/rpc?apikey=ddaba84e-c7ff-45da-8d21-7b8e25c79fe3";
+                const paymasterClient = createPimlicoPaymasterClient({
+                    transport: http(BUNDLER_PAYMASTER_URL),
+                    entryPoint: ENTRYPOINT_ADDRESS_V07,
+                });
+
+                const pimlicoBundlerClient = createPimlicoBundlerClient({
+                    transport: http(BUNDLER_PAYMASTER_URL),
+                    entryPoint: ENTRYPOINT_ADDRESS_V07,
+                });
+
+                const kernelClient = createKernelAccountClient({
+                    account,
+                    chain: baseSepolia,
+                    entryPoint: ENTRYPOINT_ADDRESS_V07,
+                    bundlerTransport: http(BUNDLER_PAYMASTER_URL),
+                    middleware: {
+                        sponsorUserOperation: paymasterClient.sponsorUserOperation, // optional
+                        gasPrice: async () => (await pimlicoBundlerClient.getUserOperationGasPrice()).fast, // use pimlico bundler to get gas prices
+                    },
+                });
+                setAccountClient(kernelClient);
+
+                console.log("smart wallet address :", account.address);
+            } catch (error) {
+                console.error("Error initializing account:", error);
+            }
         };
-        attest();
-    }, [accountClient]);
-    const attestSchema = async () => {
-        console.log("hereeeeee");
+
+        initializeAccount();
+    }, [wallets]);
+
+    // const attestSchema = async (github_url: string, maintainer_github_id: string, remark: string, contributor_github_id: string) => {
+    //     const tx = await attestSchemaInBlockchain(github_url, maintainer_github_id, remark, contributor_github_id);
+    //     if (tx !== undefined) {
+    //         console.log("Transaction successful, tx:", tx);
+
+    //     }
+    // };
+
+    const attestSchemaInBlockchain = async (
+        github_url: string,
+        maintainer_github_id: string,
+        remark: string,
+        contributor_github_id: string
+    ) => {
         let schema = "uint256 score";
         // let schemaEncoded =
         const schemaEncoder = new SchemaEncoder(schema);
         const schemaUID = getSchemaUID(schema, "0x0000000000000000000000000000000000000000", true) as `0x${string}`;
         console.log("schemaUID", schemaUID);
-        const data = schemaEncoder.encodeData([{ name: "score", value: 100, type: "uint256" }]) as `0x${string}`;
-        console.log("accClinet", accountClient);
+        const data = schemaEncoder.encodeData([
+            { name: "github_url", value: github_url, type: "string" },
+            { name: "maintainer_github_id", value: maintainer_github_id, type: "string" },
+            { name: "remark", value: remark, type: "string" },
+            { name: "contributor_github_id", value: contributor_github_id, type: "string" },
+        ]) as `0x${string}`;
         if (!accountClient) {
             console.error("Account client not found");
             return;
@@ -59,6 +148,7 @@ const useEas = () => {
             });
 
             console.log("Transaction successful, tx:", tx);
+            return tx;
         } catch (error) {
             console.error("An error occurred while writing the contract:", error);
         }
