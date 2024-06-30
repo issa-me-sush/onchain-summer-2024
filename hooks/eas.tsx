b31fe8124f8d5fe3b61745a40fdf2514c55a7d7b
lazycoder1 <gautamgsabhahit@gmail.com>
2024-07-01 02:47:39 +0530

    working attestation

--- hooks/eas.tsx
+++ hooks/eas.tsx
@@ -5,6 +5,7 @@ import { SmartAccountClient } from "permissionless";
 import { ENTRYPOINT_ADDRESS_V07_TYPE } from "permissionless/types/entrypoint";
 import { KernelAccountClient } from "@zerodev/sdk";
 import { baseSepolia } from "viem/chains";
+import { easAbi } from "./easAbi";
 
 const schemaRegistryContractAddress = "0x4200000000000000000000000000000000000020"; // base sepolia
 const schemaRegistry = new SchemaRegistry(schemaRegistryContractAddress);
@@ -40,78 +41,14 @@ const useEas = () => {
                 account: accountClient.account ? accountClient.account : "0x",
                 address: eas,
                 chain: baseSepolia,
-                abi: [
-                    {
-                        inputs: [
-                            {
-                                components: [
-                                    {
-                                        internalType: "bytes32",
-                                        name: "schema",
-                                        type: "bytes32",
-                                    },
-                                    {
-                                        components: [
-                                            {
-                                                internalType: "address",
-                                                name: "recipient",
-                                                type: "address",
-                                            },
-                                            {
-                                                internalType: "uint64",
-                                                name: "expirationTime",
-                                                type: "uint64",
-                                            },
-                                            {
-                                                internalType: "bool",
-                                                name: "revocable",
-                                                type: "bool",
-                                            },
-                                            {
-                                                internalType: "bytes32",
-                                                name: "refUID",
-                                                type: "bytes32",
-                                            },
-                                            {
-                                                internalType: "bytes",
-                                                name: "data",
-                                                type: "bytes",
-                                            },
-                                            {
-                                                internalType: "uint256",
-                                                name: "value",
-                                                type: "uint256",
-                                            },
-                                        ],
-                                        internalType: "struct AttestationRequestData",
-                                        name: "data",
-                                        type: "tuple",
-                                    },
-                                ],
-                                internalType: "struct AttestationRequest",
-                                name: "request",
-                                type: "tuple",
-                            },
-                        ],
-                        name: "attest",
-                        outputs: [
-                            {
-                                internalType: "bytes32",
-                                name: "",
-                                type: "bytes32",
-                            },
-                        ],
-                        stateMutability: "payable",
-                        type: "function",
-                    },
-                ],
+                abi: easAbi,
                 functionName: "attest",
                 args: [
                     {
                         schema: schemaUID,
                         data: {
                             recipient: "0xe95C4707Ecf588dfd8ab3b253e00f45339aC3054",
-                            expirationTime: BigInt("100"),
+                            expirationTime: BigInt("1729782120075"),
                             revocable: true,
                             refUID: "0x0000000000000000000000000000000000000000000000000000000000000000",
                             data: data,
